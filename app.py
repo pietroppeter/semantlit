a108a9bf54615c48f86efe48369aec7fedd6fd3c
czephyr <alfredo.funicello@studenti.unimi.it>
2023-07-15 12:26:22 +0200

    added download of w2c files with caching

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 import streamlit as st
+import os.path
 from gensim.models import KeyedVectors
+from huggingface_hub import hf_hub_download
+
+files = ["word2vec.wordvectors", "word2vec.wordvectors.vectors.npy"]
+
+for file in files:
+    if not os.path.exists(f"word2vec/{file}"):
+        with st.spinner(f"Downloading {file}..."):
+            hf_hub_download(
+                repo_id="osiria/word2vec-light-uncased-it",
+                filename=file,
+                local_dir="word2vec",
+                local_dir_use_symlinks=False,
+            )
 
 try:
-    model = KeyedVectors.load("./word2vec/word2vec.wordvectors", mmap='r')
+    model = KeyedVectors.load("./word2vec/word2vec.wordvectors", mmap="r")
 except FileNotFoundError:
     st.error("Word2Vec file not found")
 
 SECRET_WORD = "poesia"
 if "guesses" in st.session_state:
     guesses = st.session_state.guesses
-else: 
+else:
     guesses = []
 
 if "guess" not in st.session_state:
     st.session_state.guess = ""
 
+
 def submit():
     st.session_state.guess = st.session_state.guess_input
     st.session_state.guess_input = ""
 
-_ = st.text_input(label="Guess",value="",key="guess_input",on_change=submit)
+
+_ = st.text_input(label="Guess", value="", key="guess_input", on_change=submit)
 guess = st.session_state.guess
 if guess:
     if guess in model.key_to_index:
