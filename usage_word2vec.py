81f1e29215fb7ffa467d50045798fcc2fbff6a39
czephyr <alfredo.funicello@studenti.unimi.it>
2023-07-15 11:05:40 +0200

    added venv to gitignore and print in usage file

--- usage_word2vec.py
+++ usage_word2vec.py
@@ -2,4 +2,4 @@ from gensim.models import KeyedVectors
 
 model = KeyedVectors.load("./word2vec/word2vec.wordvectors", mmap='r')
 
-model.most_similar("poesia", topn=5)
+print(model.most_similar("poesia", topn=5))
